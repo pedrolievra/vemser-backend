94c9d551c5bdd386072f1d70a0797a19e47b01db
Pedro <pedrolievra@gmail.com>
2022-06-20 13:28:50 -0400

    corrigindo outros exercicios

--- modulo 1/src/Exercicio7.java
+++ modulo 1/src/Exercicio7.java
@@ -2,12 +2,12 @@
 public class Exercicio7 {
     public static void main(String[] args) {
 
-        int a = 20;
-        int b = 10;
+            int a = 20;
+            int b = 10;
+            int c = a;
 
-        a = b;
-        b = a;
-
-        System.out.println("a = " + a + "   b = " + b);
-    }
+            a = b;
+            b = c;
+            System.out.println("a = " + a + " e b = " + b);
+        }
 }

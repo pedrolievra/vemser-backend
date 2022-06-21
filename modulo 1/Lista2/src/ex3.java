8b977203043fa6abcff31b0ebc57a6dd1d4ac9e9
Pedro <pedrolievra@gmail.com>
2022-06-21 10:07:57 -0400

    enviando o restante dos exercicios

--- modulo 1/Lista2/src/ex3.java
+++ modulo 1/Lista2/src/ex3.java
@@ -1,11 +1,50 @@
 import java.util.Scanner;
 
 public class ex3 {
+    public static void main(String[] args) {
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-            System.out.println("Digite o nome do jogador:");
-            String[] Nome;
+        String nome , nomeMaisPesado ;
+        double altura = 0, jogadorMaisAlto = 0, jogadorMaisPesado = 0, somaDasAlturas = 0, mediaDasAlturas = 0, peso = 0;
+        int idade, jogadorMaisVelho = 0, qtdJogadores = 0, i = 1;
+
+        while (true) {
+            System.out.print("Digite o nome do " + i + "º jogador: ");
+            nome = sc.nextLine();
+            if ("SAIR".equals(nome)) {
+                break;
+            }
+            System.out.print("Digite a altura do jogador: ");
+            altura = sc.nextDouble();
+            System.out.print("Digite a idade do jogador: ");
+            idade = sc.nextInt();
+            System.out.print("Digite o peso do jogador: ");
+            peso = sc.nextDouble();
+            sc.nextLine();
+            qtdJogadores++;
+            i++;
+
+            if (altura > jogadorMaisAlto) {
+                jogadorMaisAlto = altura;
+            }
+            if (idade > jogadorMaisVelho) {
+                jogadorMaisVelho = idade;
+            }
+            if (peso > jogadorMaisPesado) {
+                jogadorMaisPesado = peso;
+                nomeMaisPesado = nome;
+            }
+            somaDasAlturas += altura;
+
+            System.out.println();
         }
+        mediaDasAlturas = somaDasAlturas / qtdJogadores;
+
+        System.out.println("Quantidade de jogadores: " + qtdJogadores);
+        System.out.println("Altura do jogador maior: " + jogadorMaisAlto);
+        System.out.println("Jogador mais velho: " + jogadorMaisVelho);
+        System.out.println("Jogador mais pesado: " + jogadorMaisPesado);
+        System.out.println("Média das alturas: " + mediaDasAlturas);
+    }
 }
--- modulo 1/Lista2/src/ex8.java
+++ modulo 1/Lista2/src/ex8.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class ex8 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int linhas = 5, coluna = 4, maiorNota = 0, matriculaNerd = 0, somaDasNotas = 0;
+        int[][] alunos = new int[linhas][coluna];
+
+        for(int i = 0; i < linhas; i++){
+            System.out.print("Digite as informações do" + (i+1) + "° aluno");
+            for(int j = 0; j < (coluna - 1); j++){
+                if(j == 0){
+                    System.out.print("Número de Matrícula: ");
+                    alunos[i][j] = sc.nextInt();
+                }
+                else if(j == 1){
+                    System.out.print("Valor da Média das provas: ");
+                    alunos[i][j] = sc.nextInt();
+                }
+                else if(j == 2){
+                    System.out.print("Valor da Média dos trabalhos: ");
+                    alunos[i][j] = sc.nextInt();
+                }
+            }
+        }
+        for(int i = 0; i < linhas; i++){
+            alunos[i][3] = (int)(alunos[i][1] * 0.6 + alunos[i][2] * 0.4);
+            somaDasNotas += alunos[i][3];
+
+            if(alunos[i][3] > maiorNota){
+                maiorNota = alunos[i][3];
+                matriculaNerd = alunos[i][0];
+            }
+        }
+        double mediaNotasFinais = (double) somaDasNotas / linhas;
+        System.out.print("Matrícula com a maior nota: " + matriculaNerd);
+        System.out.println("Nota: " + maiorNota);
+        System.out.print("Média das notas finais: " + mediaNotasFinais);
     }
 }
